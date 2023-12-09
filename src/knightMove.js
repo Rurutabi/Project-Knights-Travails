b59c3b2df4eb3008f74f9a5b7a6982e0b97933f1
Sira <siraphop_2538@hotmail.com>
2023-12-09 12:48:01 +1100

    Placing knight function

--- src/knightMove.js
+++ src/knightMove.js
@@ -1,5 +1,75 @@
 export class knightMove {
+  //Grid
+  allGrid = document.querySelectorAll('.grid-item');
+
+  //button
+  placeButton = document.querySelector('.place-knight');
+  clearButton = document.querySelector('.clear');
+  randomButton = document.querySelector('.random-knight');
+
+  placeMode = false;
+  isKnightOnBoard = false;
+
+  chessboard = Array.from({ length: 8 }, () =>
+    Array.from({ length: 8 }, () => '')
+  );
+
   constructor() {
-    console.log('Testing');
+    this.switchKnight();
+    this.switchOff();
+    this.placeKnight();
+  }
+
+  //Place Knight on a grid
+  placeKnight() {
+    this.allGrid.forEach((element, index) => {
+      element.addEventListener('click', (e) => {
+        if (this.placeMode === true && this.isKnightOnBoard === false) {
+          const img = document.createElement('img');
+          img.classList.add('knight-icon');
+          img.src = 'images/knighticon.png';
+
+          element.appendChild(img);
+
+          this.isKnightOnBoard = true;
+          //Update array where the horse icon is
+          this.updateArray(element, index);
+        }
+      });
+    });
+  }
+
+  updateArray(element, index) {
+    const row = Math.floor(index / 8);
+    const col = index % 8;
+    this.chessboard[row][col] = 'Icon';
+    console.log(this.chessboard);
+  }
+
+  //Click on place knight button to allow placing
+  switchKnight() {
+    this.placeButton.addEventListener('click', (e) => {
+      this.placeMode = true;
+    });
+  }
+
+  switchOff() {
+    this.clearButton.addEventListener('click', (e) => {
+      this.chessboard = this.chessboard.map((row) => row.map(() => ''));
+      this.removeKnight();
+      this.isKnightOnBoard = false;
+      this.placeMode = false;
+      console.log(this.chessboard);
+    });
+  }
+
+  /*Helper Method*/
+
+  //Remove KnightIcon
+  removeKnight() {
+    const knightIcon = document.querySelectorAll('.knight-icon');
+    knightIcon.forEach((element) => {
+      element.remove();
+    });
   }
 }
